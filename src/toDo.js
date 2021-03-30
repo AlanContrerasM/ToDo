246e64698e324b0d797193e98fba8d7d325e19d1
Alan Contreras <alanrcontrerasm@gmail.com>
2021-03-30 15:44:44 -0700

    Did Prettier formatting

--- src/toDo.js
+++ src/toDo.js
@@ -1,56 +1,66 @@
 //A factory function
 
-const ToDo = (title, description, dueDate, priority, notes)=>{
+const ToDo = (title, description, dueDate, priority, notes) => {
+  const getTitle = () => title;
+  const getDesc = () => description;
+  const getDueDate = () => dueDate;
+  const getPriority = () => priority;
+  const getNotes = () => notes;
 
-    const getTitle = () => title;
-    const getDesc = () => description;
-    const getDueDate = () => dueDate;
-    const getPriority = () => priority;
-    const getNotes = () => notes;
+  return { getTitle, getDesc, getDueDate, getPriority, getNotes };
+};
 
+const ToDoCheck = (
+  title,
+  description,
+  dueDate,
+  priority = 5,
+  notes,
+  checklist
+) => {
+  const { getTitle, getDesc, getDueDate, getPriority, getNotes } = ToDo(
+    title,
+    description,
+    dueDate,
+    priority,
+    notes
+  );
 
-    return{getTitle, getDesc, getDueDate, getPriority, getNotes};
+  const getChecklist = () => checklist;
 
-}
+  return { getTitle, getDesc, getDueDate, getPriority, getNotes, getChecklist };
+};
 
-const ToDoCheck = (title, description, dueDate, priority = 5, notes, checklist)=>{
+const ToDoProject = (title, toDoArr) => {
+  let _list = [];
+  if (Array.isArray(toDoArr)) {
+    _list = toDoArr;
+  } else {
+    _list.push(toDoArr);
+  }
+  const getTitle = () => title;
+  const setTitle = (newTitle) => {
+    title = newTitle;
+  };
+  const getList = () => _list;
+  const setList = (list) => {
+    _list = list;
+  };
+  const addToDo = (toDo) => {
+    _list.push(toDo);
+  };
+  const removeToDo = (task) => {
+    let indexOfTask;
+    _list.forEach((ToDo, index) => {
+      if (ToDo.getTitle() == task) {
+        indexOfTask = index;
+      }
+    });
 
-    const {getTitle, getDesc, getDueDate, getPriority, getNotes} = 
-        ToDo(title, description, dueDate, priority, notes);
-    
-        const getChecklist = () => checklist;
+    _list.splice(indexOfTask, 1);
+  };
 
-    return{getTitle, getDesc, getDueDate, getPriority, getNotes, getChecklist};
-}
+  return { getTitle, setTitle, getList, setList, addToDo, removeToDo };
+};
 
-const ToDoProject = (title, toDoArr) =>{
-    let _list = [];
-    if(Array.isArray(toDoArr)){
-        _list = toDoArr;
-    }else{
-        _list.push(toDoArr);
-    }
-    const getTitle = () => title;
-    const setTitle = (newTitle) =>{ title = newTitle};
-    const getList = () => _list;
-    const setList = (list) => {_list = list};
-    const addToDo = (toDo) =>{
-        _list.push(toDo);
-    }
-    const removeToDo = (task) =>{
-        let indexOfTask;
-        _list.forEach((ToDo, index)=>{
-            if(ToDo.getTitle() == task){
-                indexOfTask = index;
-            }
-        })
-
-        _list.splice(indexOfTask,1);
-    }
-
-    return {getTitle, setTitle, getList, setList, addToDo, removeToDo}
-
-
-}
-
-export {ToDo, ToDoCheck, ToDoProject}
+export { ToDo, ToDoCheck, ToDoProject };
